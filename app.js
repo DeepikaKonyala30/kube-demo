72d9faffc970976c9bfb71b577db3f9fb4de2374
DeepikaKonyala30 <deepikakonyala30@gmail.com>
2025-11-05 12:23:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-
+const http = require('http');
+const os = require('os');
+const server = http.createServer((req, res) => {
+  if (req.url === '/health') return res.end('OK');
+  res.end(`Hello from SHA-LOCKED Ubuntu 24.04!\nHost: ${os.hostname()}\nBuild: ${process.env.BUILD_NUMBER}`);
+});
+server.listen(3001, () => console.log('App LIVE on port 3001'));
